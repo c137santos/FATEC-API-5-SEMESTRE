46721daef84987428a7584cc14ad6b321f8dcafd
Jean <jean.roodrigues@outlook.com>
2025-09-15 22:27:48 -0300

    chore (#38): Add routes configuration frontend

--- frontend/src/router/core.routes.js
+++ frontend/src/router/core.routes.js
@@ -1,17 +1,37 @@
-// Composables
+
+
 import DefaultLayout from "@/layouts/default/DefaultLayout.vue"
 import CardListView from "@/pages/core/CardListView.vue"
+import DashboardByProject from "@/pages/core/DashboardByProject"
+import Issues from "@/pages/core/Issues"
+import Overview from "@/pages/core/Overview"
+import User from "@/pages/core/User"
 
 export default [
   {
-    path: "/cards",
+    path: "/",
     component: DefaultLayout,
     children: [
       {
-        path: "list",
-        name: "cards-list",
-        component: CardListView,
+        path: "overview",
+        name: "overview",
+        component: Overview,
       },
+      {
+        path: "dashboard-by-project",
+        name: "dashboard-by-project",
+        component: DashboardByProject,
+      },
+      {
+        path: "issues",
+        name: "issues",
+        component: Issues,
+      },
+      {
+        path: "user",
+        name: "user",
+        component: User,
+      }
     ],
   },
 ]
