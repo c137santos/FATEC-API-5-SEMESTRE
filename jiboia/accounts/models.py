706672c59144df2de7ac5bb5942d6ed06109f83a
santosc137 <cs@moray.ai>
2025-09-19 21:45:43 -0300

    chore (#55): Remove not necessary field

--- jiboia/accounts/models.py
+++ jiboia/accounts/models.py
@@ -3,8 +3,6 @@ from django.db import models
 
 
 class User(AbstractUser):
-    bio = models.TextField(null=True, blank=True)
-    avatar = models.URLField(max_length=1024, null=True, blank=True)
 
     def __str__(self):
         return str(self.username)
@@ -17,8 +15,6 @@ class User(AbstractUser):
             "first_name": self.first_name,
             "last_name": self.last_name,
             "email": self.email,
-            "avatar": self.avatar,
-            "bio": self.bio,
             "permissions": {
                 "ADMIN": self.is_superuser,
                 "STAFF": self.is_staff,
