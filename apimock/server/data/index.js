6d6327c07758b8826d463a508419a28de4320f73
Matheus Marciano <mm.leitea@outlook.com>
2025-09-22 23:11:10 -0300

    chore (#36): fix rebase

--- apimock/server/data/index.js
+++ apimock/server/data/index.js
@@ -2,6 +2,5 @@ const utils = require('../utils')
 
 module.exports = {
   users: utils.parseJson('./data/users.json'),
-  cards: utils.parseJson('./data/cards.json'),
   projectsOverview: utils.parseJson('./data/projects/overview.json')
 }
