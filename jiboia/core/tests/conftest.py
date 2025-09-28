9cb1eb034521066ee854991515dc75d4e82978bb
YanYamim <yanyamim@gmail.com>
2025-09-27 22:20:48 -0300

    refactor(#65): removing unecessary mocks

--- jiboia/core/tests/conftest.py
+++ jiboia/core/tests/conftest.py
@@ -7,7 +7,7 @@ MOCK_TODAY = date(2025, 9, 24)
 
 @pytest.fixture
 def mock_project_model():
-    """Simulate the classs Project."""
+    """Simulate the class Project."""
     class MockProject:
         def __init__(self, id, name, start_date_project):
             self.id = id
@@ -17,20 +17,20 @@ def mock_project_model():
 
 @pytest.fixture
 def mock_issue_model():
-    """Simulate the classs Issue."""
+    """Simulate the class Issue."""
     class MockIssue:
-        def __init__(self, id, project_id, created_at):
+        def __init__(self, id, project_id, created_at, status_name=None):
             self.id = id
             self.project_id = project_id
             self.created_at = created_at
+            self.status = MagicMock(name=status_name) 
             self.project = MagicMock(id=project_id) 
-            self.statuslog_set = MagicMock() 
 
     return MockIssue
 
 @pytest.fixture
 def mock_timelog_model():
-    """Simulate the classs TimeLog."""
+    """Simulate the class TimeLog."""
     class MockTimeLog:
         def __init__(self, id, id_issue_id, id_user_id, id_user__username, seconds):
             self.id = id
@@ -41,28 +41,16 @@ def mock_timelog_model():
     return MockTimeLog
 
 @pytest.fixture
-def mock_statuslog_model():
-    """Simulate the classs StatusLog."""
-    class MockStatusLog:
-        def __init__(self, id, id_issue, new_status_name):
-            self.id = id
-            self.id_issue = id_issue
-            self.new_status = MagicMock(name=new_status_name) 
-    return MockStatusLog
-
-@pytest.fixture
 def mock_managers():
     with (
         patch("jiboia.core.service.project_svc.Project.objects") as mock_project_manager,
         patch("jiboia.core.service.project_svc.Issue.objects") as mock_issue_manager,
         patch("jiboia.core.service.project_svc.TimeLog.objects") as mock_timelog_manager,
-        patch("jiboia.core.service.project_svc.StatusLog.objects") as mock_statuslog_manager,
     ):
         yield {
             "project": mock_project_manager,
             "issue": mock_issue_manager,
             "timelog": mock_timelog_manager,
-            "statuslog": mock_statuslog_manager,
         }
         
 @pytest.fixture
