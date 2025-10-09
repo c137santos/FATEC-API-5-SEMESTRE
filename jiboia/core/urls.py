eb37982b5ee76767bb803ca4938e8994e84b86ad
YanYamim <yanyamim@gmail.com>
2025-10-09 16:56:42 -0300

    test(#111): add testes of view list_project_general

--- jiboia/core/urls.py
+++ jiboia/core/urls.py
@@ -4,8 +4,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("issues/add", views.add_issue, name="add_issue"),
-    path("issues", views.list_paginable_issues, name="list_paginable_issues"),
+    path("issues/add", views.add_issue, name='add_issue'),
+    path("issues", views.list_paginable_issues, name='list_paginable_issues'),
     path("projects/<int:project_id>", views.project_overview, name='project_overview'),
-    path("projects/overview", views.list_projects_general),
+    path("projects/overview", views.list_projects_general, name='list_projects_general'),
 ]
