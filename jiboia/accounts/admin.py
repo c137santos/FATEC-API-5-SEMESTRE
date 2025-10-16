e82e9fab7a780e2a1ac0e8e0af0212ab150ecbc7
Marilia Moraes <marilia.bm01@gmail.com>
2025-10-16 20:26:33 -0300

    refactor(#97): remove UserProfile model and related admin integration

--- jiboia/accounts/admin.py
+++ jiboia/accounts/admin.py
@@ -1,18 +1,5 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
 
-from .models import User, UserProfile
+from .models import User
 
-
-class UserProfileInline(admin.StackedInline):
-    model = UserProfile
-    can_delete = False
-    verbose_name_plural = "Perfil"
-
-
-class CustomUserAdmin(UserAdmin):
-    inlines = (UserProfileInline,)
-
-
-admin.site.register(User, CustomUserAdmin)
-admin.site.register(UserProfile)
+admin.site.register(User)
