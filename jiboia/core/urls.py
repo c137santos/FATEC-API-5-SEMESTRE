0d4963e308d9c07ef6c639a57e7071d2230f2f83
Jean <jean.roodrigues@outlook.com>
2025-10-22 16:17:35 -0300

    feat(#52): add cron button logic backend

--- jiboia/core/urls.py
+++ jiboia/core/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path("projects/<int:project_id>", views.project_overview, name="project_overview"),
     path("projects/overview", views.list_projects_general, name="list_projects_general"),
     path("projects/<int:project_id>/desenvolvedores", views.project_developers, name="project_developers"),
+    path("btncron-all", views.trigger_jira_sync, name='trigger_jira_sync'),
 ]
