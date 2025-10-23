2d990dc769eb54bf7d4e1a7038074742c4a29662
YanYamim <yanyamim@gmail.com>
2025-10-23 15:30:00 -0300

    fix(#131): fixing requests of issue's list on production environment

--- jiboia/core/urls.py
+++ jiboia/core/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path("issues/add", views.add_issue, name="add_issue"),
-    path("issues", views.list_paginable_issues, name="list_paginable_issues"),
+    path("projects/<int:project_id>/issues", views.list_paginable_issues, name="list_paginable_issues"),
     path("projects/<int:project_id>", views.project_overview, name="project_overview"),
     path("projects/overview", views.list_projects_general, name="list_projects_general"),
     path("projects/<int:project_id>/desenvolvedores", views.project_developers, name="project_developers"),
