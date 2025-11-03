63346dddea849b4c53d2d9e949cb6ccfcf018618
YanYamim <yanyamim@gmail.com>
2025-11-03 13:00:58 -0300

    feat(#161): implementing view component and route to users

---
+++ frontend/src/pages/accounts/UsersView.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-container class="fill-height">
+    <ListagemUsers />
+  </v-container>
+</template>
+
+<script>
+import ListagemUsers from "@/components/ListagemUsers.vue"
+
+export default {
+  name: "UsersList",
+  components: { ListagemUsers },
+}
+</script>
+
+<style scoped>
+.done {
+  text-decoration: line-through;
+}
+</style>
