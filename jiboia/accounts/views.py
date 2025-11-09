4fe924dcb4359ae8ae1254cdd319637a581c18a1
Isaque <isaquesc2@gmail.com>
2025-11-09 11:55:10 -0300

    fix(#158): Post requirement added

--- jiboia/accounts/views.py
+++ jiboia/accounts/views.py
@@ -6,6 +6,7 @@ import logging
 from django.contrib import auth
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_http_methods
 
 from jiboia.accounts.services import create_user as create_user_service
 
@@ -57,7 +58,8 @@ def whoami(request):
     return JsonResponse(user_data)
 
 
-@csrf_exempt  # NOSONAR
+@csrf_exempt
+@require_http_methods(["POST"])
 def create_user(request):
     try:
         json.loads(request.body)
