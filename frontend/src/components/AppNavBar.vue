e2e606765da2a46719d58526736df0ae5cbd29c7
Jean <jean.roodrigues@outlook.com>
2025-11-23 18:51:40 -0300

    fix(#207): Fix UX NavBar

--- frontend/src/components/AppNavBar.vue
+++ frontend/src/components/AppNavBar.vue
@@ -2,18 +2,15 @@
   <v-app-bar app>
     <v-app-bar-nav-icon @click="drawer = !drawer" />
 
-    <v-app-bar-title>{{ title }}</v-app-bar-title>
+    <v-btn :to="{ name: 'overview'}">{{ title }}</v-btn>
 
     <template #append>
-      <v-btn icon="mdi-heart" :to="{ name: 'overview' }"></v-btn>
-      <v-btn icon="mdi-magnify"></v-btn>
       <v-btn
         :prepend-icon="theme === 'light' ? 'mdi-weather-sunny' : 'mdi-weather-night'"
         @click.stop="themeClick"
       ></v-btn>
-
-      <v-btn icon="mdi-dots-vertical">
-        <v-icon icon="mdi-dots-vertical" />
+      <v-btn icon="mdi-account">
+        <v-icon icon="mdi-account" />
         <v-menu activator="parent">
           <v-list>
             <v-list-item :to="{ name: 'accounts-logout' }">Sair</v-list-item>
@@ -25,12 +22,38 @@
 
   <v-navigation-drawer v-model="drawer" app>
     <v-list>
-      <v-list-item to="/accounts/users">Usuários</v-list-item>
+      <v-list-item to="/projects/overview">Overview</v-list-item>
+      <v-list-item to="/user">Usuários</v-list-item>
+    <v-list-group
+      append-icon="mdi-chevron-down"
+      no-action
+    >
+      <template #activator="{ props }">
+        <v-list-item v-bind="props">
+          <v-list-item-title>Projetos</v-list-item-title>
+        </v-list-item>
+      </template>
+      <template v-if="projectList.length === 0">
+      </template>
+      <v-list-item
+        v-for="project in projectList"
+        :key="project.project_id"
+        :to="`/projects/${project.project_id}`"
+        link
+      >
+        <v-list-item-content>
+          <v-list-item-title>{{ project.name }}</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+    </v-list-group>
     </v-list>
   </v-navigation-drawer>
 </template>
 
 <script>
+import { ref, onMounted } from 'vue'
+import projectsApi from '@/api/projects.api'
+
 export default {
   props: {
     title: {
@@ -44,14 +67,23 @@ export default {
       default: "dark",
     },
   },
-  emits: ["themeClick"],
-  data: () => ({
-    drawer: false,
-  }),
-  methods: {
-    themeClick() {
-      this.$emit("themeClick")
-    },
+  setup(props, { emit }) {
+    const drawer = ref(false)
+    const projectList = ref([])
+
+    onMounted(async () => {
+      try {
+        const overview = await projectsApi.overview()
+        projectList.value = overview.projects
+      } catch (error) {
+        console.error('Erro ao carregar projetos:', error)
+      }
+    })
+
+    const themeClick = () => emit("themeClick")
+
+    return { drawer, projectList, themeClick, title: props.title, theme: props.theme }
   },
 }
+
 </script>
